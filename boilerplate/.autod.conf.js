7d8c5d4e84602c8b963625be82216fb97365df2f
TZ <atian25@qq.com>
2016-11-12 10:05:11 +0800

    feat: copy from simple, remove sth

---
+++ boilerplate/.autod.conf.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  test: [
+    'test',
+    'benchmark',
+  ],
+  dep: [
+  ],
+  devdep: [
+    'egg-ci',
+    'egg-bin',
+    'autod',
+    'eslint',
+    'eslint-config-egg',
+    'supertest',
+    'power-assert',
+    'intelli-espower-loader',
+    'webstorm-disable-index',
+  ],
+  exclude: [
+    './test/fixtures',
+    './dist',
+  ],
+};
+
---
+++ boilerplate/test/app/controller/home.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const request = require('supertest');
+const mm = require('egg-mock');
+const assert = require('power-assert');
+
+describe('test/app/controller/home.test.js', () => {
+  let app;
+  before(() => {
+    app = mm.app();
+    return app.ready();
+  });
+
+  after(() => app.close());
+
+  afterEach(mm.restore);
+
+  it('should GET /', () => {
+    assert(app.name === '{{name}}');
+
+    return request(app.callback())
+    .get('/')
+    .expect('hi, egg')
+    .expect(200);
+  });
+});
